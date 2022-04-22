1491300c725b7a0fca2ba09ca166f20485efe4da
Darshan <91721723+Dasheya@users.noreply.github.com>
2022-04-22 13:08:07 +0530

    Conditions in MySQL
    All condition performed with understanding

---
+++ ConditionPract.sql
@@ -0,0 +1,44 @@
+use practicehere;
+
+create table cond(id int PRIMARY KEY,name varchar(20),surname varchar(20),address varchar(20),district varchar(20),JerseyNo int);
+
+insert into cond values(1,'rohit','sharma','navimumbai','mumbai',45);
+insert into cond values(2,'virat','kohli','nashikroad','nashik',18);
+insert into cond values(3,'saurav','ganguly','navi','kolkata',41);
+insert into cond values(4,'shikhar','dhavan','kurla','mumbai',31);
+
+alter table cond add JerseyNo varchar(20) not null;
+alter table cond drop column JerseyNo;
+
+select * from cond;
+drop table cond;
+
+UPDATE cond SET name='Trent',surname='boult',address='NewZeland',district='Awkland' where id=3;	
+
+delete from cond where id=3;
+ 
+select *  from cond where name='rohit' and JerseyNo=45;
+
+select * from cond where name='virat' or JerseyNo=17;
+
+select name from cond where name LIKE '%hit';
+select JerseyNo from cond where JerseyNo LIKE '4%';
+
+select * from cond where JerseyNo IN(45,18);
+select * from cond where name IN('rohit','virat');
+
+select * from cond where name NOT IN('rohit');
+select * from cond where district NOT IN('nashik');
+
+select * from cond where district !="nashik";
+
+select * from cond where name IS NULL;
+select * from cond where name IS NOT NULL;
+
+select * from cond where id between 2 AND 4;
+
+
+
+
+
+
